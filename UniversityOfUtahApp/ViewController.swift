5a0619750a2cc1e0fa5c8251487d0fc9a005c7b6
Mohammad Zulqurnain <mohammadzulqurnain@mohammads-macbook-air.local>
2016-08-23 01:30:20 +0500

    Initial Commit

---
+++ UniversityOfUtahApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UniversityOfUtahApp
+//
+//  Created by Mohammad Zulqurnain on 23/08/2016.
+//  Copyright © 2016 Mohammad Zulqurnain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
